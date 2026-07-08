d1c49447ecbaa1221463b1116ea63b9476aadef8
Alex <sindeyualexandr@gmail.com>
2026-07-08 17:16:46 +0300

    test2

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from PySide6.QtWidgets import QApplication
 
 
 from app.app import AppLogic
+"""Вся логика приложения написана в /app/app.py----"""
 """Вся логика приложения написана в /app/app.py"""
-"""Вся логика приложения написана в /app/app.py"""
-"""BUGFIX"""
+"""BUGFIX2"""
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = AppLogic()
